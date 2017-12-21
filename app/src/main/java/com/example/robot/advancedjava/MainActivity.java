b39e488a6d1771579db49e54ef18d96880dd0291
alay.mahlees <alay.mahlees@gmail.com>
2017-12-21 09:20:23 +0200

    first commit with android studio

---
+++ app/src/main/java/com/example/robot/advancedjava/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.robot.advancedjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btntoast);
+        final String s = "hi there, this is toast";
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this , s, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
